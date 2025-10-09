14afdaf223f5afe565f01a837ee34c852aa47cb1
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-10-09 23:21:28 +0530

    Simple interest

---
+++ DailyPractice/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+using DailyPractice;
+
+int res = SimpleINterest.CalculateSI(); // Static method call without class instance
+Console.WriteLine("Simple interest is {0}", res);
+
---
+++ DailyPractice/SimpleINterest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    internal class SimpleINterest
+    {
+
+        public static int CalculateSI() 
+        { 
+            int Principal = 1000; 
+            int Rate = 5;
+            int Time = 3;
+            return (Principal * Rate * Time) / 100;
+        }
+
+    }
+}

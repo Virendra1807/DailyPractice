cc6551bb5e4bfbd6a17b17b2b376544aa51a822a
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-10-09 23:28:26 +0530

    Boxing Unboxing in C#

---
+++ DailyPractice/BoxingUnboxing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    internal class BoxingUnboxing
+    {
+
+        // Boxing means converting a value type to an object type
+        // Unboxing means converting an object type to a value type
+
+        // Class is a blue print of an object
+
+        public void BoxUnbox()
+        {
+            int num = 123; 
+            object obj = num;
+            Console.WriteLine("Boxed value: " + obj);
+            int unboxedNum = (int)obj;
+            Console.WriteLine("Unboxed value: " + unboxedNum);
+        }
+
+    }
+}

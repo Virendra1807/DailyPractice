88ff24dfbe9766e89158058f0dca8a7f492e680f
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-10-30 22:47:12 +0530

    sum of digits

---
+++ DailyPractice/SumofDigits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    public class SumofDigits
+    {
+       public int sumofDigits()
+        {
+            int num = 25062024;
+            int sum = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                num /= 10;
+                sum += digit;
+            }
+
+            return sum;
+        }
+
+    }
+}

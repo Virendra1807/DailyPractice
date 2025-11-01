463a9035aa3fbeb162279e22015a0d4280d771b0
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-02 00:42:11 +0530

    ENcapsulation Hides the data, not a implementation

---
+++ DailyPractice/Encapsulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    internal class Encapsulation
+    {
+        // Private field - hidden from outside
+        private int age;
+
+        // Public property - provides controlled access
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0)
+                    age = value;
+                else
+                    Console.WriteLine("Age must be positive!");
+            }
+        }
+    }
+}

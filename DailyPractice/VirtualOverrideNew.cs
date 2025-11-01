f0b54ca1561d2f7f9519f88e875a5856ba85e592
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-02 00:41:05 +0530

    Use or keywords Virtual, Override, new -> for hiding parent method to override it

---
+++ DailyPractice/VirtualOverrideNew.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    
+    public class Parent
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Parent Display");
+        }
+    }
+
+    public class Child : Parent
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Child Override Display");
+        }
+    }
+
+    public class AnotherChild : Parent
+    {
+        public new void Display()
+        {
+            Console.WriteLine("AnotherChild Hidden Display");
+        }
+    }
+
+
+
+
+
+    
+}

f7a1bfa0f4a8fb5361642538bc587e80d4290ca4
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-01 21:52:19 +0530

    Private Constructor in C#

---
+++ DailyPractice/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        // Private constructor
+        private Singleton()
+        {
+            Console.WriteLine("Private constructor called");
+        }
+
+        // Public static method to access instance
+        public static Singleton GetInstance()
+        {
+            Console.WriteLine("GetInstance called");
+            if (_instance == null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+
+
+        // Output: Private constructor called (only once)
+
+    }
+
+
+
+}

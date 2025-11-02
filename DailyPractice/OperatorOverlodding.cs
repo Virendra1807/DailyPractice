3cad7827216e3849aa77b38aa32d287d04f14312
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-02 11:49:38 +0530

    Operator Overloading --> Sometime we need built in operators to work according to our logic so we use Operator overloading

---
+++ DailyPractice/OperatorOverlodding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    using System;
+
+    public class OperatorOverloadding
+    {
+        int a, b;
+        public OperatorOverloadding(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public static OperatorOverloadding operator +(OperatorOverloadding Obj1, OperatorOverloadding Obj2)
+        {
+            return new OperatorOverloadding(Obj1.a + Obj2.a, Obj1.b + Obj2.b);
+        }
+
+
+        public override string ToString() => $"({a}, {b})";
+
+    }
+
+}

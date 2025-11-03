c472e68a13096f02917bfaa69c611b748b352708
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-03 20:34:27 +0530

    if we want to restrict merthod after overriding in child class then we can use SEALED keyword to restrict the overriding of that method

---
+++ DailyPractice/RestrictMethodToOverride.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPractice
+{
+    internal class RestrictMethodToOverride
+    {
+
+    }
+
+    public class MainClass
+    {
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("Parent message");
+        }
+    }
+
+    public class SubClass : MainClass
+    {
+        public override void ShowMessage() //Sealed means the child of SubClass cant Override this method
+        {
+            Console.WriteLine("Child message");
+        }
+    }
+
+    public class GrandSubClass : SubClass
+    {
+        public new void ShowMessage() //new hides the parent menthod, and if type is parent class then it calls parent method 
+        {
+            Console.WriteLine("grand child message");
+        }
+    }
+
+
+}
+

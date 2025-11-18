fabfc1815667ee77aa7aa8051082968944f0c3d3
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-18 22:21:52 +0530

    Using OrderBy, Skip, and Take extensions

--- DailyPractice/LINQ_Methods.cs
+++ DailyPractice/LINQ_Methods.cs
@@ -10,24 +10,31 @@ namespace DailyPractice
     {
         public LINQ_Methods() { }
 
-        static int[] numbers = { 1,2,3,4,5,6,7,8,9};
+        static int[] numbers = { 1,8,3,4,7,6,29,10,9};
 
         public static void LinqExample()
         {
             var evenNumbers = numbers.Where(n => n % 2 == 0).ToList<int>();
-            Console.WriteLine("Even Numbers:");
+            //Console.WriteLine("Even Numbers:");
+
+            //foreach (var num in evenNumbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //var result = numbers.Where(n => n % 2 == 0).Select(n => n);
+            //foreach (var num in result)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            var LargestThreeNumbers = numbers.OrderByDescending(n => n).Take(3).ToList<int>();
+            var SmallestThreeNumbers = numbers.OrderByDescending(n => n).Skip(numbers.Count() - 3).ToList<int>();
 
 
-            foreach (var num in evenNumbers)
-            {
-                Console.WriteLine(num);
-            }
 
-            var result = numbers.Where(n => n % 2 == 0).Select(n => n);
-            foreach (var num in result)
-            {
-                Console.WriteLine(num);
-            }
+
+
         }
 
     }

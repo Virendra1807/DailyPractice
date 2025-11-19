32ba859c0cc5adbbf014a850e0871d3b40899c9d
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2025-11-19 23:23:46 +0530

    Bubble sort

--- DailyPractice/SortArray.cs
+++ DailyPractice/SortArray.cs
@@ -13,7 +13,7 @@ namespace DailyPractice
         {
             //int[] asc = arr;
 
-            for(int i =0; i<arr.Length-1; i++)
+            for(int i=0; i < arr.Length-1; i++)
             {
                 for (int j=i+1; j < arr.Length; j++)
                 {
